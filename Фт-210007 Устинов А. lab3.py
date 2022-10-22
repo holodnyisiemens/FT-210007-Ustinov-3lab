534be8649deaee78692b80944051c080d9c34ad9
Алексей Устинов <102724749+holodnyisiemens@users.noreply.github.com>
2022-10-22 21:18:14 +0500

    Update Фт-210007 Устинов А. lab3.py

--- Фт-210007 Устинов А. lab3.py
+++ Фт-210007 Устинов А. lab3.py
@@ -1,6 +1,9 @@
-a = ord('а')
-alphabet=''.join([chr(i) for i in range(a,a+6)] + [chr(a+33)] +
-    [chr(i) for i in range(a+6,a+32)])  # создание русского алфавита с добавлением буквы ё
+# a = ord('а')
+# alphabet=''.join([chr(i) for i in range(a,a+6)] + [chr(a+33)] +
+#     [chr(i) for i in range(a+6,a+32)])
+# создание русского алфавита с добавлением буквы ё
+
+alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'    # строка из букв русского алфавита
 
 def line_change (solution):
     new_string=''
@@ -16,22 +19,26 @@ def line_change (solution):
                 new_string+=string[i] # внесение символа не из русского алфавита
     return 'Расшифровка: '+ new_string + '\n'
 
+def check_line(string):
+    count=0 # счетчик кол-ва нерусских символов
+    for char in string:
+        if char.lower() in alphabet: # поиск хотя бы 1-го русского символа
+            break
+        else:
+            count+=1
+    return count
+
 print('Программа шифрует/дешифрует строку (через шифр Цезаря)\n'
       'Сначала напишите строку, а потом выберете, что с ней сделать\n'
       'Для завершения работы программы введите 0')
+
 while True: # бесконечный цикл (до отмены работы самим пользователем)
     string = input('Введите строку на русском языке (другие символы не изменятся) или 0: ')
     if string=='0':
         input('Работа программы завершена. Нажмите Enter для выхода из консоли')
         break    # введя 0 пользователь остановит работу программы
     else:
-        count=0 # счетчик кол-ва нерусских символов
-        for char in string:
-            if char.lower() in alphabet: # поиск хотя бы 1-го русского символа
-                break
-            else:
-                count+=1
-        if count == len(string): # все символы в строке нерусские, выводится ошибка
+        if check_line(string) == len(string): # все символы в строке нерусские, выводится ошибка
             print ('Ошибка. Введите хотя бы 1 русский символ\n')
             continue
         
@@ -45,7 +52,7 @@ while True: # бесконечный цикл (до отмены работы с
     while True: # обработка ввода
         solution = input('Для расшифровки введите -1, для шифровки 1:')
         if solution=='-1' or solution == '1':
-            print(line_change (solution))
+            print(line_change(solution))
             break # цикл обработки ввода обрывается при правильном вводе
         else:   # обработка ввода
             print('Ошибка. Попробуйте еще раз\n')
