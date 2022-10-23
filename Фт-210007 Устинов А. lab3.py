3cb15664dc297a6d1fa68da26336df06be990292
Алексей Устинов <102724749+holodnyisiemens@users.noreply.github.com>
2022-10-23 20:48:44 +0500

    Update Фт-210007 Устинов А. lab3.py

--- Фт-210007 Устинов А. lab3.py
+++ Фт-210007 Устинов А. lab3.py
@@ -1,9 +1,6 @@
-# a = ord('а')
-# alphabet=''.join([chr(i) for i in range(a,a+6)] + [chr(a+33)] +
-#     [chr(i) for i in range(a+6,a+32)])
-# создание русского алфавита с добавлением буквы ё
-
-alphabet = 'абвгдеёжзийклмнопрстуфхцчшщъыьэюя'    # строка из букв русского алфавита
+a = ord('а')
+alphabet=''.join([chr(i) for i in range(a,a+6)] + [chr(a+33)] +
+    [chr(i) for i in range(a+6,a+32)])  # создание русского алфавита с добавлением буквы ё
 
 def line_change (solution):
     new_string=''
@@ -17,10 +14,11 @@ def line_change (solution):
                 new_string+=alphabet[(index+int(solution)*num)%33].upper() #возврат регистра буквы
             except ValueError:
                 new_string+=string[i] # внесение символа не из русского алфавита
-    return 'Расшифровка: '+ new_string + '\n'
+    print('(рас)шифровка: '+ new_string + '\n')
+    return
 
 def check_line(string):
-    count=0 # счетчик кол-ва нерусских символов
+    count=0 # счетчик кол-ва символов не из русского алфавита
     for char in string:
         if char.lower() in alphabet: # поиск хотя бы 1-го русского символа
             break
@@ -38,21 +36,23 @@ while True: # бесконечный цикл (до отмены работы с
         input('Работа программы завершена. Нажмите Enter для выхода из консоли')
         break    # введя 0 пользователь остановит работу программы
     else:
-        if check_line(string) == len(string): # все символы в строке нерусские, выводится ошибка
+        non_russian_characters = check_line(string)
+        if non_russian_characters == len(string): # все символы в строке нерусские, выводится ошибка
             print ('Ошибка. Введите хотя бы 1 русский символ\n')
             continue
         
-    while True:
-        try: # обработка ввода
-            num = int(input('Введите целое число смещений по алфавиту: '))
+    while True: # цикл обработки ввода
+        step = input('Введите целое число смещений по алфавиту: ')
+        try: 
+            num = int(step)
             break
         except ValueError:
-            print('Ошибка ввода. Попробуйте еще раз')
+            print('Ошибка ввода. Попробуйте еще раз\n')
             
-    while True: # обработка ввода
+    while True: # цикл обработки ввода
         solution = input('Для расшифровки введите -1, для шифровки 1:')
         if solution=='-1' or solution == '1':
-            print(line_change(solution))
+            line_change(solution)
             break # цикл обработки ввода обрывается при правильном вводе
         else:   # обработка ввода
             print('Ошибка. Попробуйте еще раз\n')
